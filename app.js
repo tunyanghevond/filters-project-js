0a5f7308cfab57a8e87df5fb345688a67f58a388
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-04-07 20:37:41 +0200

    add html structure and displayProduct function

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('filters project');
+let filteredProduct = [...products];
+const productContainer  = document.querySelector('.products-container');
+
+function displayProducts(){
+productContainer.innerHTML = filteredProduct.map(product =>{
+    const {id,title,company,image,price} = product
+    return ` <img src="${image}" class="product-img img">
+          <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">${price}</span>
+          </footer>`
+})
+}
+displayProducts();

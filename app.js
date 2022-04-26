f42072589508f03cabac336252dc6207318bcfab
Ghevond Tunyan <ghtunyan2018@gmail.com>
2022-04-26 17:46:37 +0200

    add search input funtionality

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 let filteredProduct = [...products];
+
 const productContainer  = document.querySelector('.products-container');
 
 function displayProducts(){
-productContainer.innerHTML = filteredProduct.map(product =>{
-    const {id,title,company,image,price} = product
-    return ` <img src="${image}" class="product-img img">
+
+productContainer.innerHTML = filteredProduct.map(({id,title,image,price}) =>{
+    
+    return `<article class="product" data-id='${id}'>
+          <img src="${image}" alt="" class="product-img img">
           <footer>
             <h5 class="product-name">${title}</h5>
             <span class="product-price">${price}</span>
-          </footer>`
+          </footer>
+        </article>`;
 })
-}
+.join('');
+};
+
 displayProducts();
+
+// text filter
+
+const form = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
+
+form.addEventListener('keyup',function(){
+
+ const inputValue = searchInput.value;
+
+ filteredProduct = products.filter(product =>{
+   return product.title.toLowerCase().includes(inputValue)
+ })
+ displayProducts();
+});
